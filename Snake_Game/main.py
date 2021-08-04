3205e502ae12eae4b1173c3f1e5f56586aa951f4
abanerjee-06 <aritra.b2000@outlook.com>
2021-08-04 23:01:51 +0300

    Another project

---
+++ Snake_Game/main.py
@@ -0,0 +1,7 @@
+level = 25 # 10,60,120
+win_size = (900,560)
+colors = [pygame.Color(0,0,0),pygame.Color(255,255,255)
+pygame.init()
+
+win = pygame.set_mode(win_size)
+pygame.display.set_caption('Snake Game in Python')
