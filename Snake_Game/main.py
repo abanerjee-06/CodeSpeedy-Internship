f364ecc40ba723f138b4278f43864e53f3d7e72e
abanerjee-06 <aritra.banerjee@students.iiit.ac.in>
2021-09-08 09:06:48 +0300

    Removed old folders and files

--- Snake_Game/main.py
+++
@@ -1,7 +0,0 @@
-level = 25 # 10,60,120
-win_size = (900,560)
-colors = [pygame.Color(0,0,0),pygame.Color(255,255,255),pygame.Color(255,0,0),pygame.Color(0,255,0),pygame.Color(0,0,255)]
-pygame.init()
-
-win = pygame.set_mode(win_size)
-pygame.display.set_caption('Snake Game in Python')
